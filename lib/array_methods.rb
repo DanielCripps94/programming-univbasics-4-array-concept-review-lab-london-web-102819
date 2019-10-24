6c8c5557a73557cb6bbcf4e06eaf6522db791065
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-24 11:59:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,5 +17,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-
+  min = array[0]
+  length = array.length
+  length.times do |index|
+    if min > array[index]
+      min = array[index]
+end
+  return min
 end
