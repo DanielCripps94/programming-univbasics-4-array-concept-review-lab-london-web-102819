29f4c130e2e2862c243e24a754002da4593be6f1
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-24 12:00:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,7 @@ def find_element_index(array, value_to_find)
       return index
     end
   return nil
-end
+
 
 def find_max_value(array)
     length = array.length
@@ -14,7 +14,7 @@ def find_max_value(array)
       max = array[index]
   end
   return max
-end
+
 
 def find_min_value(array)
   min = array[0]
@@ -24,4 +24,4 @@ def find_min_value(array)
       min = array[index]
 end
   return min
-end
+
